27e0236f76878b1d912dbc6887251228e6350809
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-10-19 15:25:25 +0300

    2

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+'use strict';
+console.log("Hello JS");
+const name = "Alex";
+const age = 33;
+const studdyJs = true;
+let time = "14.00";
+
+console.log("I know need study JS", studdyJs);
+
+const type = typeof age;
+console.log(type);
+
+alert(time);
+
+// window.confirm() window.prompt() we can use only confirm and promt
+
+const shouldStuddy = confirm("Do you want know JS?");
+console.log(shouldStuddy);
+
+const period = prompt('How many times You need?');
+
+console.log(period);
+console.log(typeof period)
