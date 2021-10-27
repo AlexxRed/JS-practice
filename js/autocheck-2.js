3253308bd6399e04a2fd6b4c8f4c3703a864db2d
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-10-27 12:07:40 +0300

    Update autocheck-2.js

--- js/autocheck-2.js
+++ js/autocheck-2.js
@@ -319,3 +319,12 @@ calculateTotal(24) //возвращает 300
 calculateTotal()
 // ===============================================================================================
 
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+for (let i = 0; i < fruits.length; i+=1) { // Change this line
+  const fruit = fruits[i]; // Change this line
+  console.log(fruit);
+}
+
+// ================================================================================================
+
