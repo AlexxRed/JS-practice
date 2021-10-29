6a7779916db64f57e9ca106a3923782d466d4d36
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-10-29 19:16:18 +0300

    finish AutoCheck 2

--- js/autocheck-2.js
+++ js/autocheck-2.js
@@ -377,5 +377,248 @@ findLongestWord("Google do a roll") //возвращает Google
 findLongestWord("May the force be with you") //возвращает force
 
 // ==================================================================================================
+//22
+function createArrayOfNumbers(min, max) {
+  const numbers = [];
+  // Change code below this line
+  for (let i = min; i <= max; i += 1) {
+    numbers.push(i);
+    console.log(numbers);
+  };
+  // Change code above this line
+  return numbers;
+}
+createArrayOfNumbers(1, 3) //возвращает [1, 2, 3]
+createArrayOfNumbers(14, 17) //возвращает [14, 15, 16, 17]
+createArrayOfNumbers(29, 34) //возвращает [29, 30, 31, 32, 33, 34]
+// =====================!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!++++=========
+
+function filterArray(numbers, value) {
+   // Change code below this line
+  const arr = [];
+  
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] > value) {
+    arr.push(numbers[i])
+    console.log(arr);
+  }
+}
+  
+return arr;
 
 
+  // Change code above this line
+}
+
+filterArray([1, 2, 3, 4, 5], 3) //возвращает [4, 5]
+filterArray([1, 2, 3, 4, 5], 4) //возвращает [5]
+filterArray([1, 2, 3, 4, 5], 5) //возвращает []
+filterArray([12, 24, 8, 41, 76], 38) //возвращает [41, 76]
+filterArray([12, 24, 8, 41, 76], 20) //возвращает [24, 41, 76]
+// ====================================================================================================
+
+function checkFruit(fruit) {
+  const fruits = ["apple", "plum", "pear", "orange"];
+console.log(fruits.includes(fruit));
+  return fruits.includes(fruit); // Change this line
+}
+
+checkFruit("plum") //возвращает true
+checkFruit("mandarin") //возвращает false
+checkFruit("pear") //возвращает true
+checkFruit("Pear") //возвращает false
+checkFruit("apple") //возвращает true
+checkFruit() //со случайным словом возвращает правильное значение boolean
+// ===================================================================================================
+
+function getCommonElements(array1, array2) {
+  // // Change code below this line
+  // let finishArray = [];
+  // const totalArray = array1.concat(array2);
+  // console.log(totalArray);
+  // // for (let i = 0; i < totalArray.length; i += 1) {
+  // //   if (totalArray[i] === totalArray[i]) {
+  // //     finishArray.push(totalArray[i]);
+  // //     console.log(finishArray);
+  // //   }
+  // // }
+  // for (const num of totalArray) {
+  //   if (num === num) {
+  //     finishArray.push(num);
+  //     console.log(finishArray);
+  //   }
+  // }
+  let finishArray = [];
+for (const element of array1) {
+   if(array2.includes(element)){
+     finishArray.push(element);
+     console.log(finishArray);
+   }
+}
+  return finishArray;
+
+ // Change code above this line
+}
+getCommonElements([1, 2, 3], [2, 4]) //возвращает [2]
+getCommonElements([1, 2, 3], [2, 1, 17, 19]) //возвращает [1, 2]
+getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]) //возвращает [12, 27, 3]
+getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]) //возвращает [10, 30, 40]
+getCommonElements([1, 2, 3], [10, 20, 30]) //возвращает []
+
+// =================================================================================================
+
+function calculateTotalPriceI(order) {
+  let total = 0;
+  // Change code below this line
+
+  for (const numb of order) {
+    total += numb;
+    console.log(total);
+  }
+
+  // Change code above this line
+  return total;
+}
+calculateTotalPriceI([12, 85, 37, 4]) //возвращает 138
+calculateTotalPriceI([164, 48, 291]) //возвращает 503
+calculateTotalPriceI([412, 371, 94, 63, 176]) //возвращает 1116
+calculateTotalPriceI([]) //возвращает 0
+
+// ==================================================================================================
+
+function filterArrayII(numbers, value) {
+  // Change code below this line
+  const filteredNumbers = [];
+
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   const number = numbers[i];
+
+  //   if (number > value) {
+  //     filteredNumbers.push(number);
+  //   }
+  // }
+  
+  for (const number of numbers) {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+
+  return filteredNumbers;
+  // Change code above this line
+}
+
+filterArrayII([1, 2, 3, 4, 5], 3) //возвращает [4, 5]
+filterArrayII([1, 2, 3, 4, 5], 4) //возвращает [5]
+filterArrayII([1, 2, 3, 4, 5], 5) //возвращает []
+filterArrayII([12, 24, 8, 41, 76], 38) //возвращает [41, 76]
+filterArrayII([12, 24, 8, 41, 76], 20) //возвращает [24, 41, 76]
+
+// ==================================================================================================
+const a = 3 % 3;
+const b = 4 % 3;
+const c = 11 % 8;
+const d = 12 % 7;
+const e = 8 % 3;
+
+// Значение переменной a это число 0
+// Объявлена переменная b
+// Значение переменной b это число 1
+// Объявлена переменная c
+// Значение переменной c это число 3
+// Объявлена переменная d
+// Значение переменной d это число 5
+// Объявлена переменная e
+// Значение переменной e это число 2
+// ===================================================================================================
+
+
+function getEvenNumbers(start, end) {
+
+let array = [];
+  for (let i = start; i <= end; i += 1) {
+    if(i % 2 === 0){
+    array.push(i);
+    }
+  };
+  console.log(array);
+
+  return array;
+    
+}
+  
+getEvenNumbers(2, 5) //возвращает [2, 4]
+getEvenNumbers(3, 11) //возвращает [4, 6, 8, 10]
+getEvenNumbers(6, 12) //возвращает [6, 8, 10, 12]
+getEvenNumbers(8, 8) //возвращает [8]
+getEvenNumbers(7, 7) //возвращает []
+
+// ===================================================================================================
+
+// Дополни код так, чтобы в переменную number записывалось первое число от start до end,
+  // которое делится на 5 без остатка.
+
+const startI = 6;
+const endI = 27;
+let numberI;
+
+for (let i = startI; i < endI; i += 1) {
+  if (i % 5 === 0) {
+    numberI = i;
+    break;
+  }
+}
+console.log(numberI);
+
+//Объявлена переменная start со значением 6
+//Объявлена переменная end со значением 27
+//Объявлена переменная number без инициализации
+//Итоговое значение переменной number равно 10
+
+// ====================================================================================================
+
+function findNumber(start, end, divisor) {
+  // Change code below this line
+
+  for (let i = start; i < end; i += 1) {
+    if (i % divisor === 0) {
+      console.log(i);
+      return i;
+     
+    }
+  }
+  // Change code above this line
+}
+
+//Вызов findNumber(2, 6, 5) возвращает 5
+findNumber(8, 17, 3) //возвращает 9
+findNumber(6, 9, 4) //возвращает 8
+findNumber(16, 35, 7) //возвращает 21
+findNumber() //со случайным набором чисел возвращает верный результат
+//В цикле for не должен использоваться break для выхода до завершения всех итераций цикла
+
+// ===================================================================================================
+
+function includes(arr, value) {
+  // Change code below this line
+for ( const numb of arr) {
+  if (numb === value){
+    return true;
+  } 
+  
+}
+  return false;
+  // Change code above this line
+}
+
+
+includes([1, 2, 3, 4, 5], 3) //возвращает true
+includes([1, 2, 3, 4, 5], 17) //возвращает false
+includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter") //возвращает true
+includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus") //возвращает false
+includes(["apple", "plum", "pear", "orange"], "plum")//возвращает true
+includes(["apple", "plum", "pear", "orange"], "kiwi") //возвращает false
+includes() //для случайного массива со случайным value возвращает верный boolean
+//В функции includes используется for, return, но не метод массива includes
+
+// ==================================================================================================
