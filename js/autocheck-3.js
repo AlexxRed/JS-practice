a5b8bce19776a312abc90797403d88f8be8eaac2
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-10-30 16:51:40 +0300

    Update autocheck-3.js

--- js/autocheck-3.js
+++ js/autocheck-3.js
@@ -10,11 +10,11 @@
 
 
 const apartmentII = {
- imgUrl:"https://via.placeholder.com/640x480",
- descr: "Spacious apartment in the city center",
- rating: 4,
- price: 2153,
- tags: ["premium", "promoted", "top"],
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
 };
 
 // ====================================task_2==========================================================
@@ -173,4 +173,187 @@ const aptITags = apartment["tags"];
 // Значение переменной aptTags это ["premium", "promoted", "top"]
 
 // ====================================task_6==========================================================
+
+// Дополни код обновив значения свойств объекта apartment:
+
+// цену в свойстве price на 5000;
+// рейтинг квартиры в свойстве rating на 4.7;
+// имя владельца во вложенном свойстве name на "Henry Sibola";
+// массив тегов в свойстве tags добавив в конец строку "trusted".
+
+const apartmentIV = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+apartmentIV.price = 5000;
+apartmentIV.rating = 4.7;
+apartmentIV.owner.name = "Henry Sibola";
+apartmentIV.tags.push("trusted");
+// Change code below this line
+// Объявлена переменная apartment
+// Значение переменной apartment это объект
+// Значение вложенного свойства price это число 5000
+// Значение вложенного свойства rating это число 4.7
+// Значение вложенного свойства name это строка "Henry Sibola"
+// Значение вложенного свойства tags это массив ["premium", "promoted", "top", "trusted"]
+
+// ====================================task_7==========================================================
+
+// Добавь объекту apartment несколько новых свойств:
+// area - площадь в квадратных метрах, число 60;
+// rooms - количество комнат, число 3;
+// location - местоположение квартиры, обьект со следующими вложенными свойствами;
+// country - страна, строка "Jamaica";
+// city - город, строка "Kingston".
+
+const apartmentV = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4.7,
+  price: 5000,
+  tags: ["premium", "promoted", "top", "trusted"],
+  owner: {
+    name: "Henry Sibola",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+apartmentV.area = 60;
+apartmentV.rooms = 3;
+apartmentV.location = {
+ country: "Jamaica",
+ city: "Kingston",
+}
+console.log(apartmentV);
+// Change code below this line
+
+
+// Объявлена переменная apartment
+// Значение переменной apartment это объект
+// Значение вложенного свойства area это число 60
+// Значение вложенного свойства rooms это число 3
+// Значение вложенного свойства location это объект
+// Значение вложенного свойства location.country это строка "Jamaica"
+// Значение вложенного свойства location.city это строка "Kingston"
+
+// ====================================task_8==========================================================
+// Дополни код объявления объекта так, чтобы у него были свойства name, price, image и tags
+//  со значениями из переменных с аналогичными именами.Обязательно используй синтаксис коротких свойств.
+
+  const namen = "Repair Droid";
+const pricen = 2500;
+const imagen = "https://via.placeholder.com/640x480";
+const tagsn = ["on sale", "trending", "best buy"];
+
+const productV = {
+  // Change code below this line
+  namen,
+  pricen,
+  imagen,
+  tagsn,
+  // Change code above this line
+  };
+ console.log(productV);
+  
+//  Объявлена переменная product
+// Значение переменной product это объект
+// Значение вложенного свойства name это строка "Repair Droid"
+// Значение вложенного свойства price это число 2500
+// Значение вложенного свойства image это строка "https://via.placeholder.com/640x480"
+// Значение вложенного свойства tags это массив ["on sale", "trending", "best buy"]
+
+// ====================================task_9==========================================================
+
+//+++++++++++++++++++++theoryyy++++++++++++++
+const propName = "name";
+const user = {
+  age: 25,
+  // Имя этого свойства будет взято из значения переменной propName
+  [propName]: "Генри Сибола"
+};
+
+console.log(user.name); // "Генри Сибола"
+console.log(user);
+//++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Дополни код объявления объекта credentials так, чтобы в результате у него были два свойства:
+//  email и password, имена которых хранятся в переменных emailInputName и passwordInputName.
+// Значением свойства email должна быть строка "henry.carter@aptmail.com",
+//  а значением свойства password - строка "jqueryismyjam".
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
+  // Change code below this line
+[emailInputName]: "henry.carter@aptmail.com",
+[passwordInputName]: "jqueryismyjam",
+
+
+  // Change code above this line
+};
+console.log(credentials);
+
+// Объявлена переменная credentials
+// Значение переменной credentials это объект
+// В объекте credentials есть свойство email
+// Значение вложенного свойства email это строка "henry.carter@aptmail.com"
+// В объекте credentials есть свойство password
+// Значение вложенного свойства password это строка "jqueryismyjam"
+
+// ====================================task_10==========================================================
+
+//+++++++++++++++++++++theoryyy++++++++++++++
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  rating: 8.38,
+};
+
+for (const key in book) {
+  // Ключ
+  console.log(key);
+  // Значение свойства с таким ключом
+  console.log(book[key]);
+}
+//++++++++++++++++++++++++++++++++++++++++++++
+
+// Перебери объект apartment используя цикл for...in
+//   и запиши в массив keys все его ключи, а в массив values все значения его свойств.
+const apartmentN = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const keysN = [];
+const valuesN = [];
+// Change code below this line
+
+for (const item in apartmentN) {
+keysN.push(item);
+valuesN.push(apartment[item]);
+}
+console.log(keysN);
+console.log(valuesN);
+
+// Объявлена переменная apartment
+// Значение переменной apartment это объект
+// Объявлена переменная keys
+// Значение переменной keys это массив ["descr", "rating", "price"]
+// Объявлена переменная values
+// Значение переменной values это массив ["Spacious apartment in the city center", 4, 2153]
+
+// ====================================task_11==========================================================
+
 
